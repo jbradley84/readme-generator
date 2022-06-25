c28906bcfe25c972d0c437b43043e9d925c0615d
jbradley84 <utbradley84@yahoo.com>
2022-06-25 12:56:26 -0500

    created new src dist directories

--- index.js
+++ index.js
@@ -52,16 +52,80 @@ const promptReadme = () => {
          name: "problemSolution",
          message: "What solution does your project offer to the problem?",
          when: ({ confirmProblem }) => confirmProblem
+      },
+      {
+         type: "confirm",
+         name: "projectSite",
+         message: "Has your project been deployed?",
+         default: true
+      },
+      {
+         type: "input",
+         name: "deployedSite",
+         message: "Provide a link to your deployed project site.",
+         when: ({ projectSite }) => projectSite
+      },
+      {
+         type: "input",
+         name: "projectSnip",
+         message: "Provide a link to a screenshot of your project."
+      },
+      {
+         type: "input",
+         name: "installation",
+         message: "How can users install your application?"
+      },
+      {
+         type: "list",
+         name: "license",
+         message: "Choose a licesne to add to your project.",
+         choices: ["1", "2", "3", "4"]
+      },
+      {
+         type: "confirm",
+         name: "contributors",
+         message: "Did other developers contribute to your project?",
+         default: false
+      },
+      {
+         type: "input",
+         name: "contributorsList",
+         message: "Provide a link to the GitHub page of the contributing developer.",
+         when: ({ contributors }) => contributors
+      },
+      {
+         type: "input",
+         name: "contribute",
+         message: "Let users know how they can contribute to your project."
+      },
+      {
+         type: "confirm",
+         name: "projectTests",
+         message: "Have you created application tests for users to run?",
+         default: false
+      },
+      {
+         type: "input",
+         name: "tests",
+         message: "Provide a link to the test."
+      },
+      {
+         type: "input",
+         name: "github",
+         message: "Provide a link to your GitHub profile."
+      },
+      {
+         type: "input",
+         name: "email",
+         message: "Provide an email address where users can contact you with questions."
       }
    ])
-   //.then(console.log(promptReadme));
 };
 
 
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
